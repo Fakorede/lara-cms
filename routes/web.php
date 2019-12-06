6eb4c1476c57e9c9288c07c2e43030ba8b6b8ef1
fakorede <abiolafakorede@gmail.com>
2019-12-06 06:47:09 +0100

    setting up backend layout

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/blog/{post}', 'BlogController@show')->name('show');
 
 Route::get('/category/{category}', 'BlogController@category')->name('category');
 Route::get('/author/{author}', 'BlogController@author')->name('author');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
