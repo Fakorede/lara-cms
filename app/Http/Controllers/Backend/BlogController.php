1a8bf26e1d2566141327587d49b9c08d34fabdb3
fakorede <abiolafakorede@gmail.com>
2019-12-10 15:12:16 +0100

    upload post image

--- app/Http/Controllers/Backend/BlogController.php
+++ app/Http/Controllers/Backend/BlogController.php
@@ -9,6 +9,13 @@ use App\Http\Requests\PostRequest;
 class BlogController extends BackendController
 {
     protected $limit = 5;
+    protected $uploadPath;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->uploadPath = public_path('img');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -39,8 +46,28 @@ class BlogController extends BackendController
      */
     public function store(PostRequest $request)
     {
-        $request->user()->posts()->create($request->all());
+        $data = $this->handleRequest($request);
+        $request->user()->posts()->create($data);
+
         return redirect('/backend/blog/')->with('message', 'Post created successfully!');
+    }
+
+    private function handleRequest($request)
+    {
+        $data = $request->all();
+
+        if($request->hasFile('image')) 
+        {
+            $image = $request->file('image');
+            $fileName = $image->getClientOriginalName();
+            $destination = $this->uploadPath;
+
+            $image->move($destination, $fileName);
+
+            $data['image'] = $fileName;
+        }
+
+        return $data;
     }
 
     /**
